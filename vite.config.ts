20ba69c738dd7d424fab7f611c898d4abed93da7
Olivier Dupont <olivier.dupont@twin.life>
2023-06-09 12:45:10 +0200

    Feature #6: Configuration du projet
    - ssl est nécessaire pour utiliser correctement les API MediaDevice en développement 
    - Ajout de la configuration LAN pour le développement

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,18 @@ import { defineConfig } from "vite";
 import checker from "vite-plugin-checker";
 
 // https://vitejs.dev/config/
+import basicSsl from "@vitejs/plugin-basic-ssl";
+
 export default defineConfig({
+	server: {
+		host: true,
+	},
 	plugins: [
 		react(),
 		checker({
 			// e.g. use TypeScript check
 			typescript: true,
 		}),
+		basicSsl(),
 	],
 });
