35c087179c2eae4c50637f288d5810456c137bb8
Stephane Carrez <Stephane.Carrez@twin.life>
2024-03-21 08:30:20 +0100

    Build #55: Package
      Modified files:
        o package-lock.json
        o package.json
          Change version to 1.4.4
        o vite.config.ts
          Record the {build: minify} configuration as esbuild so that I remember where/how to setup this setting.

--- vite.config.ts
+++ vite.config.ts
@@ -24,6 +24,9 @@ export default defineConfig({
         // }),
         basicSsl(),
     ],
+    build: {
+        minify: 'esbuild'
+    },
 	define: {
 		__APP_VERSION__: JSON.stringify(process.env.npm_package_version),
 	},
