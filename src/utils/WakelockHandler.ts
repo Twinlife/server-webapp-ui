750d82fb9b27209ffd956b5f80e265a8041f5931
Romain Kolb <romain.kolb@skyrock.com>
2024-10-16 09:30:58 +0200

    Feature #66: Le browser se met en background pendant l'appel
    New files:
      o src/utils/WakelockHandler.ts
        Manage wakelock acquisition and release.
    
    Modified files:
      o src/calls/CallService.ts
        Add wakelock property.
        In actionOutgoingCall(), acquire the wakelock.
        In onSessionTerminate() and onTerminatePeerConnection(), release the wakelock if the call is finished.

---
+++ src/utils/WakelockHandler.ts
@@ -0,0 +1,39 @@
+/*
+ *  Copyright (c) 2024 twinlife SA.
+ *
+ *  All Rights Reserved.
+ *
+ *  Contributors:
+ *   Romain Kolb (romain.kolb@skyrock.com)
+ */
+
+export class WakelockHandler {
+	private wakeLock: WakeLockSentinel | null = null;
+
+	public acquire() {
+		try {
+
+			this.release();
+
+			navigator.wakeLock.request("screen").then(
+				(wakeLock: WakeLockSentinel) => {
+					this.wakeLock = wakeLock;
+					console.log("Wakelock acquired");
+				},
+				(reason: any) => console.error("Could not acquire wakeLock", reason)
+			);
+		} catch (err) {
+			// the wake lock request fails - usually system related, such being low on battery
+			console.log("Could not acquire wakelock", err);
+		}
+	}
+
+	public release() {
+		if (this.wakeLock !== null) {
+			this.wakeLock.release().then(
+				() => console.log("Wakelock released"),
+				(reason) => console.error("Could not release wakelock", reason)
+			);
+		}
+	}
+}
