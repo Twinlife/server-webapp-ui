e9e25f2b22167988f484a61430b352322179cad4
Stephane Carrez <Stephane.Carrez@twin.life>
2025-06-17 15:19:57 +0200

    Feature #81: Preparation mise en Open Source
      Modified files:
        o src/calls/CallConnection.ts
        o src/calls/CallObserver.ts
        o src/calls/CallParticipant.ts
        o src/calls/CallParticipantEvent.ts
        o src/calls/CallParticipantObserver.ts
        o src/calls/CallService.ts
        o src/calls/CallState.ts
        o src/calls/CallStatus.ts
        o src/calls/ConnectionOperation.ts
        o src/calls/ConversationService.ts
        o src/calls/ErrorType.ts
        o src/calls/OnPushIQ.ts
        o src/calls/ParticipantInfoIQ.ts
        o src/calls/ParticipantTransferIQ.ts
        o src/calls/PushObjectIQ.ts
        o src/calls/PushTwincodeIQ.ts
        o src/components/Alert.tsx
        o src/components/DraggableParticipant.tsx
        o src/components/GuestNameForms.tsx
        o src/components/InitializationPanel.tsx
        o src/components/LocalParticipant.tsx
        o src/components/ParticipantGridCell.tsx
        o src/components/ParticipantsGrid.tsx
        o src/components/SelectDevicesButton.tsx
        o src/components/Thanks.tsx
        o src/components/WhiteButton.tsx
        o src/components/chatbox/ChatBox.tsx
        o src/components/chatbox/ChatBoxInput.tsx
        o src/components/chatbox/InvitationDialog.tsx
        o src/components/chatbox/InvitationItem.tsx
        o src/i18n/i18n.ts
        o src/main.tsx
        o src/pages/Call.tsx
        o src/pages/ErrorPage.tsx
        o src/services/ContactService.ts
        o src/services/PeerCallService.ts
        o src/utils/BinaryCompactDecoder.ts
        o src/utils/BinaryCompactEncoder.ts
        o src/utils/BinaryPacketIQ.ts
        o src/utils/ByteArrayInputStream.ts
        o src/utils/ByteArrayOutputStream.ts
        o src/utils/Decoder.ts
        o src/utils/DetectAgent.ts
        o src/utils/Encoder.ts
        o src/utils/SchemaKey.ts
        o src/utils/Serializer.ts
        o src/utils/SerializerException.ts
        o src/utils/SerializerFactory.ts
        o src/utils/UUID.ts
        o src/utils/Utf8.ts
        o src/utils/Version.ts
        o src/utils/WakelockHandler.ts
          Update the license for AGPL-3.0

--- src/calls/ConversationService.ts
+++ src/calls/ConversationService.ts
@@ -1,7 +1,6 @@
 /*
  *  Copyright (c) 2015-2024 twinlife SA.
- *
- *  All Rights Reserved.
+ *  SPDX-License-Identifier: AGPL-3.0-only
  *
  *  Contributors:
  *   Christian Jacquemot (Christian.Jacquemot@twinlife-systems.com)
--- src/utils/SchemaKey.ts
+++ src/utils/SchemaKey.ts
@@ -1,7 +1,6 @@
 /*
  *  Copyright (c) 2020 twinlife SA.
- *
- *  All Rights Reserved.
+ *  SPDX-License-Identifier: AGPL-3.0-only
  *
  *  Contributors:
  *   Christian Jacquemot (Christian.Jacquemot@twinlife-systems.com)
