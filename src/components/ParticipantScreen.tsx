28aa0aa6f80b5c6d60db5f2a784903b999815dcd
Stephane Carrez <Stephane.Carrez@twin.life>
2026-02-27 18:40:19 +0100

    Bug #101: Clignotement des vignettes vidéo pendant l'appel
      Modified file:
        o src/components/ParticipantScreen.tsx
          Update to get a participant object as parameter, don't put
          a method in the useEffect() to avoid flickering, get the
          local media stream directly from the MediaStreams global.

--- src/components/ParticipantScreen.tsx
+++ src/components/ParticipantScreen.tsx
@@ -7,21 +7,36 @@
  *   Stephane Carrez (Stephane.Carrez@twin.life)
  */
 import { useEffect, useRef } from "react";
+import { CallParticipant } from "../calls/CallParticipant";
+import { mediaStreams } from "../utils/MediaStreams";
+
+const DEBUG = import.meta.env.VITE_APP_DEBUG === "true";
 
 interface ParticipantGridCellProps {
-	setRemoteRenderer?: (remoteRenderer: HTMLVideoElement) => void;
-	participantId?: number;
+	participant: CallParticipant | null;
 	videoClick: (ev: React.MouseEvent<HTMLDivElement>, participantId: number | undefined) => void;
 }
 
-const ParticipantScreen: React.FC<ParticipantGridCellProps> = ({ setRemoteRenderer, participantId, videoClick }) => {
+const ParticipantScreen: React.FC<ParticipantGridCellProps> = ({ participant, videoClick }) => {
 	const refVideo = useRef<HTMLVideoElement>(null);
+	const participantId = participant ? participant.getParticipantId() : 0;
 
 	useEffect(() => {
-		if (refVideo.current && setRemoteRenderer) setRemoteRenderer(refVideo.current);
-	}, [setRemoteRenderer, refVideo]);
+		if (refVideo.current) {
+			if (participant) {
+				participant.setRemoteRenderer(refVideo.current, null);
+			} else {
+				refVideo.current.srcObject = mediaStreams.stream;
+			}
+		}
+		if (DEBUG) {
+			console.log("set video participant", participant);
+		}
+	}, [refVideo, participantId]);
 
-	console.error("Showing participant screen ", participantId);
+	if (DEBUG) {
+		console.log("Show participant screen", participantId);
+	}
 	return (
 		<div
 			className="relative flex w-3/4 pr-2 h-full items-center justify-center overflow-hidden rounded-lg bg-gray-900"
